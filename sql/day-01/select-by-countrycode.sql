d867e63eec0880ab4a59f202cedddfc46b72b1fc
priyanka03sawant <priyanka03sawant@gmail.com>
2025-10-18 19:19:35 +0530

    Create select-by-countrycode.sql

---
+++ sql/day-01/select-by-countrycode.sql
@@ -0,0 +1,10 @@
+-- Problem: Select By Country Code
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Description:
+-- Retrieve all columns from the CITY table
+-- where the COUNTRYCODE is 'JPN' (Japan).
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
