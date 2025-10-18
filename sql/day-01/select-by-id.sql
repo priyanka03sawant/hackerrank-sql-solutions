c8e9c1bc27b4aeeb62b7e3738fa33f0a242f6d1b
priyanka03sawant <priyanka03sawant@gmail.com>
2025-10-18 19:15:19 +0530

    Create select-by-id.sql

---
+++ sql/day-01/select-by-id.sql
@@ -0,0 +1,10 @@
+-- Problem: Select By ID
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Description:
+-- Retrieve all the columns from the CITY table 
+-- where the ID of the city is equal to 1661.
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
