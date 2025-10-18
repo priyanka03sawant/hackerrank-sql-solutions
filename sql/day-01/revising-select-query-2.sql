9aab48d3f03facec4f11e9508b071f2ae485cfa5
priyanka03sawant <priyanka03sawant@gmail.com>
2025-10-18 19:23:39 +0530

    Create revising-select-query-2.sql

---
+++ sql/day-01/revising-select-query-2.sql
@@ -0,0 +1,10 @@
+-- Problem: Revising the Select Query II
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Description:
+-- Retrieve the NAME column from the CITY table
+-- for all cities where COUNTRYCODE is 'JPN' (Japan).
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
