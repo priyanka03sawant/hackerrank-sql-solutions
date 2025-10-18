35db357afe3dd32c6852815ed362bce516e27d3a
priyanka03sawant <priyanka03sawant@gmail.com>
2025-10-18 19:17:15 +0530

    Create select-all.sql

---
+++ sql/day-01/select-all.sql
@@ -0,0 +1,9 @@
+-- Problem: Select All
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Description:
+-- Retrieve all columns and all rows from the CITY table.
+-- This is a basic query to explore the full table.
+
+SELECT *
+FROM CITY;
